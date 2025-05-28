d856a18f16a09f88fc3120032e446e522935ef4d
Smitha <smitha92005@gmail.com>
2025-05-28 15:22:25 +0530

    Simple Project

---
+++ Dice-Rolling/index.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btn");
+const myLabel1 = document.getElementById("label1");
+const myLabel2 = document.getElementById("label2");
+const myLabel3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+btn.onclick = function(){
+    randomNum1 = Math.floor(Math.random() *max) + min;
+    randomNum2 = Math.floor(Math.random() *max) + min;
+    randomNum3 = Math.floor(Math.random() *max) + min;
+    myLabel1.textContent = randomNum1;
+    myLabel2.textContent = randomNum2;
+    myLabel3.textContent = randomNum3;
+}
