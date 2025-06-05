75499dd0e8e12583cd2fe639074038b24b72b697
Smitha <smitha92005@gmail.com>
2025-06-05 07:32:56 +0530

    adding html and css

--- Random-Password-Generator/index.js
+++ Random-Password-Generator/index.js
@@ -1,4 +1,4 @@
-/RANDOM PASSWORD GENERATOR
+//RANDOM PASSWORD GENERATOR
 
 function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
 
@@ -19,7 +19,7 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
         return `password length must be atleast 1`;
     }
     if(allowedChars.length === 0){
-        return `(At least i set of charecters need to be selected)`;
+        return `(At least 1 set of charecters need to be selected)`;
     }
 
     for(let i = 0; i < length; i++){
@@ -28,19 +28,17 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
     }
 
     return password;
-
-
 }
 
-
+function handleGenerate(){
 
 const passwordLength = 12;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
-
+const includeLowercase = document.getElementById("includeLowercase").checked;
+const includeUppercase = document.getElementById("includeUppercase").checked;
+const includeNumbers = document.getElementById("includeNumbers").checked;
+const includeSymbols = document.getElementById("includeSymbols").checked;
 
 const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
 
-console.log(`Generated password: ${password}`);
+document.getElementById("output").textContent = `generated Password ${password}`;
+}
